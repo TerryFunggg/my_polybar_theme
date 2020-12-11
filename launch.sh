ce9a2993df36244801bd908a73627e6b57306e80
TerryFunggg <terryyessfung@gmail.com>
2020-12-11 15:25:35 +0800

    update

--- launch.sh
+++ launch.sh
@@ -6,6 +6,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -r bar &
+#polybar -r bar &
+
+external_monitor=$(xrandr --query | grep 'HDMI1')
+if [[ $external_monitor = *connected* ]]; then
+	polybar -r top &
+	polybar -r top_external &
+else
+	polybar -r bar &
+fi
+
 
 echo "Polybar launched..."
