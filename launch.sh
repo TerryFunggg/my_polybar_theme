f55f28e64c5a168e8c20d57ff291c3a1e2ad44ac
TerryFunggg <terryyessfung@gmail.com>
2020-12-15 10:32:54 +0800

    update

--- launch.sh
+++ launch.sh
@@ -6,15 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-#polybar -r bar &
+# some times I just want my big monitor whitout my laptop monitor
+need_dual_monitor = 0
 
+# xrandr can help you get the current active monitor info
 external_monitor=$(xrandr --query | grep 'HDMI1')
-if [ $external_monitor = *connected* ]; then
+
+if [[ $need_dual_monitor -eq 0 ]]; then
+	polybar -r bar &
+elif [[ $external_monitor = *connected* ]]; then
 	polybar -r top &
 	polybar -r top_external &
-else
-	polybar -r bar &
 fi
-
 
 echo "Polybar launched..."
